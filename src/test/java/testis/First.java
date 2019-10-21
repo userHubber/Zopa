b183d0cfff080d5cbd9180b90a90364a22af5db8
i.belyaev <i.belyaev@QA4.kupivip.local>
2019-10-21 12:14:43 +0300

    mail

--- src/test/java/testis/First.java
+++ src/test/java/testis/First.java
@@ -5,6 +5,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class First {
@@ -21,8 +23,6 @@ public class First {
 
     @BeforeClass
     public void setUpClass() throws Exception {
-
-        System.out.println("start drova");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\i.belyaev\\Desktop\\chromedriver.exe");
         driver = new ChromeDriver();
     }
@@ -30,6 +30,9 @@ public class First {
     @Test
     public void test() throws Exception {
         driver.get("https://mail.ru/");
+        WebElement webelement = driver.findElement(By.xpath("//*[text()='Mail.ru']"));
+        System.out.println(webelement.getText());
+
     }
 
     @AfterClass
